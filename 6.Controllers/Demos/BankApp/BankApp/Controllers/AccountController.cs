9b72c4e97eb19b5d75b1211e61d2f8d45929856c
Miklo1775 <thelegendofvictor@gmail.com>
2023-09-17 12:01:44 -0500

    Uploading progress for Section 6: Controllers

---
+++ 6.Controllers/Demos/BankApp/BankApp/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankApp.CustomClasses;
+using System.Runtime.CompilerServices;
+
+namespace BankApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly Object AccountDetails = new {accountNumber = 1001, accountHolderName = "Example Name", currentBalance = 5000};
+        [Route("/")]
+        public IActionResult Index()
+        {
+            Response.StatusCode = 200;
+            return Content("<h1>Welcome to the Best Bank</h1>", "text/html");
+        }
+
+        [Route("/account-details")]
+        public IActionResult GetAccountDetails()
+        {
+            Response.StatusCode = 200;
+            return Json(this.AccountDetails);
+        }
+
+        [Route("/account-statement")]
+        public IActionResult GetAccountStatement()
+        {
+            Response.StatusCode = 200;
+            return File("dummy.pdf", MIMEType.GetMIMEType("dummy.pdf"));
+
+        }
+
+        [Route("get-current-balance/{accountNumber:int?}")]
+        public IActionResult GetCurrentBalance()
+        {
+
+            int? accountNum = Convert.ToInt32(Request.RouteValues["accountNumber"]);
+            Console.WriteLine(accountNum);
+            if(accountNum != 0)
+            {
+                if(accountNum != 1001)
+                {
+                    return BadRequest("Account number should be 1001");
+                }
+
+                var type = this.AccountDetails.GetType();
+                var prop = type.GetProperty("currentBalance");
+                int balance = (int)prop!.GetValue(this.AccountDetails, null)!;
+
+                Response.StatusCode = 200;
+                return Content($"Your current balance is: {balance}", "text/plain");
+
+            }
+
+            return NotFound("Account number should be supplied.");
+
+        }
+    }
+}
---
+++ 6.Controllers/Demos/BankApp/BankApp/CustomClasses/MIMEType.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+
+namespace BankApp.CustomClasses
+{
+    public class MIMEType
+    {
+        internal static string GetMIMEType(string fileName)
+        {
+            const string DefaultContentType = "application/octet-stream";
+
+            string contentType;
+
+            var provider = new FileExtensionContentTypeProvider();
+
+            if (!provider.TryGetContentType(fileName, out contentType))
+            {
+                contentType = DefaultContentType;
+            }
+
+            return contentType;
+        }
+    }
+}
