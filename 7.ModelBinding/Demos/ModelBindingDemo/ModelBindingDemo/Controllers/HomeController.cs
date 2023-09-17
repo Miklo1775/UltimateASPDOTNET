c9913a6f0bbce0acba9454206f6f1a032990abcb
Miklo1775 <thelegendofvictor@gmail.com>
2023-09-17 13:11:01 -0500

    Uploading progress for Section 7: Model Binding

---
+++ 7.ModelBinding/Demos/ModelBindingDemo/ModelBindingDemo/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingDem.Controllers
+{
+    public class HomeController : Controller
+    {
+        //IF WE USE ROUTE PARAMETERS, THEN THE MODEL BINDING WILL FETCH THE DATA FROM THERE SINCE IT HAS A HIGHER PRIORITY THAN QUERY STRING.
+        //SO THE VALUES FROM THE ROUTE PARAMETERS WILL BE SUPPLIED TO THE ACTION METHOD UNLESS THE ROUTE PARAMETERS DONT CONTAIN THAT DATA.
+        [Route("/book/{bookId?}/{isloggedin?}")]
+        public IActionResult Index(int? bookId, bool? isloggedin)
+        {
+            //WITH MODEL BINDING, WE CAN RETRIEVE THE VALUE AS A PARAMETER IN OUR METHOD.
+            //WE SHOULD ALSO DECLARE THAT THE VARIABLES COULD BE NULL.
+            if (bookId.HasValue == false)
+            {
+                return BadRequest("Bookid is not supplied.");
+            }
+
+            if (String.IsNullOrEmpty(Convert.ToString(bookId)))
+            {
+                return BadRequest("Bookid cannot be null or empty");
+            }
+
+
+            if (bookId < 1 || bookId > 1000)
+            {
+                return NotFound("Book id can't be less than 1 or greater than 1000");
+            }
+
+            if (isloggedin == false)
+            {
+                return Unauthorized("User must be authenticated");
+            }
+
+            return Content($"Book id: {bookId}", "text/plain");
+
+        }
+
+        [Route("bookstore")]
+        public IActionResult BookStore()
+        {
+            int bookId = 3;
+            return LocalRedirectPermanent($"store/books/{bookId}");
+        }
+    }
+
+}
---
+++ 7.ModelBinding/Demos/ModelBindingDemo/ModelBindingDemo/Controllers/StoreController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultDemo.Controllers
+{
+    public class StoreController : Controller
+    {
+        [Route("store/books/{bookId}")]
+        public IActionResult Books()
+        {
+            int id = Convert.ToInt32(Request.RouteValues["bookId"]);
+            return Content($"<h1>Book ID: {id}</h1>", "text/html");
+        }
+    }
+}
