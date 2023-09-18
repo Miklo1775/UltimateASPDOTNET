3937574c841775d1021287d1c2750244bea06966
Miklo1775 <thelegendofvictor@gmail.com>
2023-09-17 19:02:46 -0500

    Uploading progress for Section 7: Model Binding

---
+++ 7.ModelBinding/Demos/ModelBindingDemo/ModelBindingDemo/Controllers/AudioBookController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ModelBindingDemo.Controllers
+{
+    public class AudioBookController : Controller
+    {
+        //THIS CONTROLLER IS FOR FromRoute and FromQuery examples.
+
+        //FOR EACH PARAMETER, WE CAN SELECT WHERE WE WANT TO PULL THE DATA FROM.
+        //FOR THE ROUTE DIRECTLY BELOW, WE ARE PULLING THE AUDIOBOOKID FROM THE ROUTEVALUE AND THE ISLOGGEDIN BOOLEAN VALUE FROM THE QUERY STRING.
+        [Route("audiobook/{audiobookId?}/{isloggedin?}")]
+        public IActionResult Index([FromRoute]int? audiobookId, [FromQuery]bool? isloggedin)
+        {
+            if (audiobookId.HasValue == false)
+            {
+                return BadRequest("Audiobookid is not supplied.");
+            }
+
+            if (String.IsNullOrEmpty(Convert.ToString(audiobookId)))
+            {
+                return BadRequest("Audioookid cannot be null or empty");
+            }
+
+
+            if (audiobookId < 1 || audiobookId > 1000)
+            {
+                return NotFound("Audiobook id can't be less than 1 or greater than 1000");
+            }
+
+            if (isloggedin == false)
+            {
+                return Unauthorized("User must be authenticated");
+            }
+
+            return Content($"Audiobook id: {audiobookId}", "text/plain");
+        }
+    }
+}
---
+++ 7.ModelBinding/Demos/ModelBindingDemo/ModelBindingDemo/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace ModelBindingDemo.Models
+{
+    public class Book
+    {
+        public int? BookId { get; set; }
+        public string? Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"Book Object - Book ID: {this.BookId}, Author: {Author}";
+        }
+    }
+}
