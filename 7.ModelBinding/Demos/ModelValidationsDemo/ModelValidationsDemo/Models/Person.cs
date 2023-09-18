92798d97482cd364a3183f8b254f7073d414b60c
Miklo1775 <thelegendofvictor@gmail.com>
2023-09-18 17:32:00 -0500

    Uploading progress for Section 7: Model Binding

--- 7.ModelBinding/Demos/ModelValidationsDemo/ModelValidationsDemo/Models/Person.cs
+++ 7.ModelBinding/Demos/ModelValidationsDemo/ModelValidationsDemo/Models/Person.cs
@@ -5,12 +5,17 @@ namespace ModelValidationsDemo.Models
     public class Person
     {
         //ALONGSIDE DEFAULT ERROR MESSAGE, WE CAN USE OUR OWN ERROR MESSAGE SUCH AS BELOW.
-        [Required(ErrorMessage = "Name of person is required")]
+        //THE DISPLAY ATTRIBUTE CAN DISPLAY THE VALUE INSTEAD OF THE PROPERTY.
+        //SO THE {0} WILL BE "Person Name" and not "PersonName".
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "Person Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} to {1} characters")]
         public string? PersonName { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
+        [Range(0, 999.99, ErrorMessage = "{0} should be a value between {1} and {2}")]
         public double? Price { get; set; }
 
         public override string ToString()
