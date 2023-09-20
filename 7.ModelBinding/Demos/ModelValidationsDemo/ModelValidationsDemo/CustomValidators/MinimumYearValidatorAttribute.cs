e9a66aabe819dd37bb2ac3c4317689e3e865da01
Miklo1775 <thelegendofvictor@gmail.com>
2023-09-20 12:56:21 -0500

    Uploading progress for Section 7: Model Binding

---
+++ 7.ModelBinding/Demos/ModelValidationsDemo/ModelValidationsDemo/CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationsDemo.CustomValidators
+{
+    public class MinimumYearValidatorAttribute : ValidationAttribute
+    {
+        public int MinimumYear { get; set; } = 2000;
+        public string DefaultErrorMessage { get; set; } = "Year should not be less than {0}";
+        //WE CAN ALSO ADD A CONSTRUCTOR TO TAKE THE VALUES FROM THE MODEL CLASS.
+        public MinimumYearValidatorAttribute()
+        {
+        }
+
+        public MinimumYearValidatorAttribute(int minimumYear)
+        {
+            this.MinimumYear = minimumYear;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+                DateTime date = (DateTime)value;
+                if(date.Year >= MinimumYear)
+                {
+                    //WE CAN ALSO PASS THE ErrorMessage VARIABLE AND SET THE VALUE WHERE WE USE THE CUSTOM ATTRIBUTE
+                    //return new ValidationResult(ErrorMessage);
+                    return new ValidationResult(string.Format(ErrorMessage ?? DefaultErrorMessage, MinimumYear));
+                }
+
+                return ValidationResult.Success;
+            }
+
+            return null;
+        }
+    }
+}
--- 7.ModelBinding/Demos/ModelValidationsDemo/ModelValidationsDemo/Models/Person.cs
+++ 7.ModelBinding/Demos/ModelValidationsDemo/ModelValidationsDemo/Models/Person.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using ModelValidationsDemo.CustomValidators;
+using System.ComponentModel.DataAnnotations;
 
 namespace ModelValidationsDemo.Models
 {
@@ -10,17 +12,35 @@ namespace ModelValidationsDemo.Models
         [Required(ErrorMessage = "{0} is required")]
         [Display(Name = "Person Name")]
         [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} to {1} characters")]
+        [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should only be alphabetical characters.")]
         public string? PersonName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Email should be a valid email format.")]
         public string? Email { get; set; }
+
+        [Phone(ErrorMessage = "{0} must be a valid phone number")]
+        //[ValidateNever]
         public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
         public string? Password { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Compare("Password", ErrorMessage = "{0} and {1} do not match")]
+        [Display(Name = "Re-enter Password")]
         public string? ConfirmPassword { get; set; }
         [Range(0, 999.99, ErrorMessage = "{0} should be a value between {1} and {2}")]
         public double? Price { get; set; }
 
+        [MinimumYearValidator(2005, ErrorMessage = "Year should be less than {0}")]
+        public DateTime? DateOfBirth { get; set; }
+
+
+
         public override string ToString()
         {
-            return $"Person Data Received:\nName: {this.PersonName}\nEmail: {this.Email}\nPhone: {this.Phone}\nPassword: {this.Password}\nConfirmed Password: {this.ConfirmPassword}\nPrice: {this.Price}";
+            return $"Person Data Received:\nName: {this.PersonName}\nEmail: {this.Email}\nPhone: {this.Phone}\nPassword: {this.Password}\nConfirmed Password: {this.ConfirmPassword}\nPrice: {this.Price}\nDate of Birth: {this.DateOfBirth}";
         }
     }
 }
