194f5a40432f4e0623969876159d3f95a25ce254
Miklo1775 <thelegendofvictor@gmail.com>
2023-09-22 10:10:44 -0500

    Uploading progress for Section 7: Model Binding

--- 7.ModelBinding/Demos/SimpleEcommerceApp/SimpleEcommerceApp/Controllers/OrderController.cs
+++ 7.ModelBinding/Demos/SimpleEcommerceApp/SimpleEcommerceApp/Controllers/OrderController.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleEcommerceApp.Models;
 
 namespace SimpleEcommerceApp.Controllers
 {
     public class OrderController : Controller
     {
         [Route("/order")]
-        public IActionResult Index()
+        public IActionResult Index([Bind(nameof(Order.InvoicePrice), nameof(Order.Products), nameof(Order.OrderDate))] [FromForm] Order order)
         {
-            return View();
+              if (!ModelState.IsValid)
+                {
+                    string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+                    return BadRequest(errors);
+                }
+
+            return Json(order);
         }
     }
 }
--- 7.ModelBinding/Demos/SimpleEcommerceApp/SimpleEcommerceApp/Models/Order.cs
+++ 7.ModelBinding/Demos/SimpleEcommerceApp/SimpleEcommerceApp/Models/Order.cs
@@ -2,7 +2,7 @@
 
 namespace SimpleEcommerceApp.Models
 {
-    public class Order
+    public class Order : IValidatableObject
     {
         public int? OrdeNo { get; set; }
         [Required]
@@ -10,6 +10,31 @@ namespace SimpleEcommerceApp.Models
         [Required]
         public double InvoicePrice { get; set; }
         [Required]
-        public List<Product>? Products { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Products.Count == 0)
+            {
+                yield return new ValidationResult("Every order needs a minimum of 1 product");
+            }
+
+            if(InvoicePrice != CalculateTotalProductPrice())
+            {
+                yield return new ValidationResult("Invoice Price does not match the total price of items");
+            }
+
+        }
+
+        private double CalculateTotalProductPrice()
+        {
+            double total = 0;
+            foreach (Product product in Products)
+            {
+                total += product.Price * product.Quantity;
+            }
+
+            return total;
+        }
     }
 }
