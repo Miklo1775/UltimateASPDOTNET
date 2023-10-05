0a257e5eac85cfc7888d695758a50b3688b4609a
Miklo1775 <thelegendofvictor@gmail.com>
2023-10-05 12:14:40 -0500

    added wwwroot folder and controller folder with Home Controller

--- 8.RazorViews/Demos/ViewsDemo/ViewsDemo/Program.cs
+++ 8.RazorViews/Demos/ViewsDemo/ViewsDemo/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+//ENABLES THE CONTROLLERS & VIEWS
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapControllers();
 
 app.Run();
