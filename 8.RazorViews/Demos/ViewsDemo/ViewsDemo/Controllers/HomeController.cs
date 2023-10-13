b90a21d7cc15ebb4dfa3ef145dd39317cdfdaa18
Miklo1775 <thelegendofvictor@gmail.com>
2023-10-12 21:04:40 -0500

    Uploading progress for Section 8: Razor Views

--- 8.RazorViews/Demos/ViewsDemo/ViewsDemo/Controllers/HomeController.cs
+++ 8.RazorViews/Demos/ViewsDemo/ViewsDemo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Reflection.PortableExecutable;
+using Microsoft.AspNetCore.Mvc;
 using ViewsDemo.Models;
 
 namespace ViewsDemo.Controllers
@@ -21,8 +22,34 @@ namespace ViewsDemo.Controllers
             // ViewData["people"] = persons;
             ViewBag.people = persons;
             ViewData["appTitle"] = "ASP.NET Core View Demo";
-            return View(); 
+            
+            //WHEN WE ARE USING A STRONGLY TYPED VIEW, WE SUPPLY THE DATA AS A PARAMETER IN THE View()
+            //IF HAVE TO SUPPLY THE VIEW NAME, WE MUST SUPPLY THE VIEW NAME BEFORE THE OBJECT.
+            return View("Index", persons); 
             //return new ViewResult() {ViewName = "abc"};
+        }
+
+        [Route("person-details/{name}")]
+        public IActionResult Details(string? name)
+        {
+            if (name is null)
+            {
+                return Content("Person name cannot be null");
+            }
+            
+            List<Person> persons = new List<Person>()
+            {
+                new Person(){Name = "Squishy", DateOfBirth = DateTime.Parse("2023-01-01"), PersonGender = Gender.Female},
+                new Person() {Name = "Chichi", DateOfBirth = DateTime.Parse("2021-04-01"), PersonGender = Gender.Female},
+                new Person(){Name = "Sylvestre", DateOfBirth = DateTime.Parse("2022-11-01"), PersonGender = Gender.Male},
+                new Person() { Name = "Kinto", DateOfBirth = null, PersonGender = Gender.Male}
+            };
+
+            Person? matchedPerson = persons.Single(temp =>
+                temp.Name?.ToLower() ==
+                name.ToLower());
+            
+            return View("Details", matchedPerson);
         }
     }
 }
