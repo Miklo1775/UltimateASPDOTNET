b6c568d8fa92b01c72768b5e16789894e5255f21
Miklo1775 <thelegendofvictor@gmail.com>
2023-10-27 13:28:30 -0500

    created a partial view for the cities

--- 10.PartialViews/Demos/WeatherAppPartialViews/WeatherAppPartialViews/Controllers/WeatherController.cs
+++ 10.PartialViews/Demos/WeatherAppPartialViews/WeatherAppPartialViews/Controllers/WeatherController.cs
@@ -1,14 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using WeatherAppPartialViews.Models;
 
 namespace WeatherAppPartialViews.Controllers;
 
 public class WeatherController : Controller
 {
+	private readonly List<CityWeather> _context = new List<CityWeather>()
+	{
+		new CityWeather()
+		{
+			CityUniqueCode = "LND", CityName = "London",
+			DateAndTime = DateTime.Parse("2030-01-01"),
+			TemperatureFahrenheit = 33
+		},
+		new CityWeather()
+		{
+			CityUniqueCode = "NYC", CityName = "New York City",
+			DateAndTime = DateTime.Parse("2030-01-01"),
+			TemperatureFahrenheit = 60
+		},
+		new CityWeather()
+		{
+			CityUniqueCode = "PAR", CityName = "Paris",
+			DateAndTime = DateTime.Parse("2030-01-01"),
+			TemperatureFahrenheit = 82
+		}
+	};
+	
 	[Route("/")]
 	[Route("weather")]
 	public IActionResult Index()
 	{
-		return View();
+		
+		return View(_context);
 	}
 
 	[Route("weather/single-city/{cityUniqueCode}")]
