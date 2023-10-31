29a25cb0cc2cccd544dad241446d4318e163898d
Miklo1775 <thelegendofvictor@gmail.com>
2023-10-31 15:49:33 -0500

    Uploading progress for Section 11: View Components

--- 11.ViewComponents/Demos/ViewComponentDemo/ViewComponentDemo/ViewComponents/GridViewComponent.cs
+++ 11.ViewComponents/Demos/ViewComponentDemo/ViewComponentDemo/ViewComponents/GridViewComponent.cs
@@ -6,23 +6,23 @@ namespace ViewComponentDemo.ViewComponents;
 // [ViewComponent]
 public class GridViewComponent : ViewComponent
 {
-	public async Task<IViewComponentResult> InvokeAsync()
+	public async Task<IViewComponentResult> InvokeAsync(PersonGridModel grid)
 	{
-		PersonGridModel model = new PersonGridModel()
-		{
-			GridTitle = "Persons",
-			Persons = new List<Person>()
-			{
-				new Person()
-					{ PersonName = "Chichi", JobTitle = "Drama Queen" },
-				new Person() { PersonName = "Cheddar", JobTitle = "Mama cat" },
-				new Person() { PersonName = "Zora", JobTitle = "Model cat" },
-				new Person() { PersonName = "Squishy", JobTitle = "Snow cat" }
-			}
-		};
+		// PersonGridModel model = new PersonGridModel()
+		// {
+		// 	GridTitle = "Persons",
+		// 	Persons = new List<Person>()
+		// 	{
+		// 		new Person()
+		// 			{ PersonName = "Chichi", JobTitle = "Drama Queen" },
+		// 		new Person() { PersonName = "Cheddar", JobTitle = "Mama cat" },
+		// 		new Person() { PersonName = "Zora", JobTitle = "Model cat" },
+		// 		new Person() { PersonName = "Squishy", JobTitle = "Snow cat" }
+		// 	}
+		// };
 		// ViewData["Grid"] = model;
 		// return View("Sample"); //INVOKES A PARTIAL VIEW FROM Views/Shared/Components/Grid/Default.cshtml
 		// BELOW WE ARE PASSING THE PERSONS TO THE VIEW AS AN OBJECT
-		return View("Sample", model);
+		return View("Sample", grid);
 	}
 }
