9c5fc114108dabb5a2ce00daa465b153eda893fc
Miklo1775 <thelegendofvictor@gmail.com>
2023-11-03 15:15:19 -0500

    Uploading progress for Section 12: Dependency Injection

--- 12.DependencyInjection/Demos/DIExample/DIExample/Controllers/HomeController.cs
+++ 12.DependencyInjection/Demos/DIExample/DIExample/Controllers/HomeController.cs
@@ -6,20 +6,28 @@ namespace DIExample.Controllers;
 
 public class HomeController : Controller
 {
-	// private readonly ICitiesService _citiesService;
-	//
-	// public HomeController(ICitiesService citiesService)
-	// {
-	// 	//CREATING OBJECT OF CitiesService
-	// 	//HOWEVER, THIS IS VERY BAD PRACTICE BECAUSE NOW THE CONTROLLER IS DEPENDENT ON THE SERVICE
-	// 	// _citiesService = new CitiesService();
-	// 	_citiesService = citiesService;
-	// }
+	private readonly ICitiesService _citiesService1;
+	private readonly ICitiesService _citiesService2;
+	private readonly ICitiesService _citiesService3;
+	
+	public HomeController(ICitiesService citiesService1, ICitiesService 
+            citiesService2, ICitiesService citiesService3)
+	{
+		//CREATING OBJECT OF CitiesService
+		//HOWEVER, THIS IS VERY BAD PRACTICE BECAUSE NOW THE CONTROLLER IS DEPENDENT ON THE SERVICE
+		// _citiesService = new CitiesService();
+		_citiesService1 = citiesService1;
+		_citiesService2 = citiesService2;
+		_citiesService3 = citiesService3;
+	}
 	
 	[Route("/")]
-	public IActionResult Index([FromServices]ICitiesService _citiesService)
+	public IActionResult Index()
 	{
-		List<string> cities = _citiesService.GetCities();
+		List<string> cities = _citiesService1.GetCities();
+		ViewBag.InstanceId1 = _citiesService1.ServiceInstanceId;
+		ViewBag.InstanceId2 = _citiesService2.ServiceInstanceId;
+		ViewBag.InstanceId3 = _citiesService3.ServiceInstanceId;
 		return View(cities);
 	}
 }
