dda2a20ddc25c9585bf9f263ba2ede1fe0f6eb92
Miklo1775 <thelegendofvictor@gmail.com>
2023-11-03 14:53:28 -0500

    Uploading progress for Section 12: Dependency Injection

--- 12.DependencyInjection/Demos/DIExample/DIExample/Controllers/HomeController.cs
+++ 12.DependencyInjection/Demos/DIExample/DIExample/Controllers/HomeController.cs
@@ -6,18 +6,18 @@ namespace DIExample.Controllers;
 
 public class HomeController : Controller
 {
-	private readonly ICitiesService _citiesService;
-
-	public HomeController(ICitiesService citiesService)
-	{
-		//CREATING OBJECT OF CitiesService
-		//HOWEVER, THIS IS VERY BAD PRACTICE BECAUSE NOW THE CONTROLLER IS DEPENDENT ON THE SERVICE
-		// _citiesService = new CitiesService();
-		_citiesService = citiesService;
-	}
+	// private readonly ICitiesService _citiesService;
+	//
+	// public HomeController(ICitiesService citiesService)
+	// {
+	// 	//CREATING OBJECT OF CitiesService
+	// 	//HOWEVER, THIS IS VERY BAD PRACTICE BECAUSE NOW THE CONTROLLER IS DEPENDENT ON THE SERVICE
+	// 	// _citiesService = new CitiesService();
+	// 	_citiesService = citiesService;
+	// }
 	
 	[Route("/")]
-	public IActionResult Index()
+	public IActionResult Index([FromServices]ICitiesService _citiesService)
 	{
 		List<string> cities = _citiesService.GetCities();
 		return View(cities);
